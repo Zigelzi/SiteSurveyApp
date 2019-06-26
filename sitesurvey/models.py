f6651e49ddfc04da10de1e508a5b2a97bdf109fd
Zigelzi <40773205+Zigelzi@users.noreply.github.com>
2019-06-26 22:10:13 +0300

    Finished the database models

--- sitesurvey/models.py
+++ sitesurvey/models.py
@@ -11,28 +11,22 @@ def load_user(user_id):
 
 # Many to many relationship tables
 
-# How organizations and organization types are related
+# Organizations and organization association table
 org_type_rel = db.Table('org_type',
                         db.Column('org_id', db.Integer, db.ForeignKey('organization.id')),
                          db.Column('type_id', db.Integer, db.ForeignKey('orgtype.id'))
                          )
 
-# How organizations and contact persons are related
+# Organizations and contact person association table
 contact_person_rel = db.Table('org_contact_person',
                         db.Column('org_id', db.Integer, db.ForeignKey('organization.id')),
                          db.Column('contact_id', db.Integer, db.ForeignKey('contactperson.id'))
                          )
 
-# How surveys and contact persons are related
+# Surveys and contact person association table
 survey_contact_rel = db.Table('survey_contact_person',
                         db.Column('survey_id', db.Integer, db.ForeignKey('survey.id')),
                          db.Column('contact_id', db.Integer, db.ForeignKey('contactperson.id'))
-                         )
-
-# How surveys and locations are related
-survey_llocation_rel = db.Table('survey_location',
-                        db.Column('survey_id', db.Integer, db.ForeignKey('survey.id')),
-                         db.Column('location_id', db.Integer, db.ForeignKey('location.id'))
                          )
 
 class User(db.Model, UserMixin):
@@ -44,10 +38,10 @@ class User(db.Model, UserMixin):
     last_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    org_id = db.Column(db.Integer(), db.ForeignKey('organization.id'))
+    org_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
 
-    # Backref to show surveys done by user surveys model
-    surveys = db.relationship('Survey', backref='user', lazy='dynamic')
+    # Backref to show surveys done by user
+    surveys = db.relationship('Survey', backref='user', lazy=True)
 
     def set_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
@@ -59,7 +53,7 @@ class User(db.Model, UserMixin):
         return f'User <{self.first_name} | {self.last_name} | {self.email}>'
 
 class Organization(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     org_name = db.Column(db.String(100), nullable=False)
     org_number = db.Column(db.String(20), nullable=False, unique=True)
     address = db.Column(db.String(100), nullable=False)
@@ -67,50 +61,57 @@ class Organization(db.Model):
     city = db.Column(db.String(30), nullable=False)
     country = db.Column(db.String(30), nullable=False)
 
+    # Organization type and contact person mapping
+    org_type = db.relationship('Orgtype', secondary=org_type_rel, backref='organizations', lazy=True)
+    contact_person = db.relationship('Contactperson', secondary=contact_person_rel, backref='organizations', lazy=True)
+
     def __repr__(self):
         return f"""Organization <{self.org_name} | {self.org_number} | {self.address} | {self.postal_code} |
                                  {self.city} | {self.country}"""
 
 class Survey(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    grid_connection = db.Column(db.Integer())
+    id = db.Column(db.Integer, primary_key=True)
+    grid_connection = db.Column(db.Integer)
     grid_cable = db.Column(db.String(15))
-    max_power = db.Column(db.Float())
-    consumtion_fuse = db.Column(db.Integer())
-    maincabinet_rating = db.Column(db.Integer())
-    empty_fuses = db.Column(db.Boolean())
-    number_of_slots = db.Column(db.Integer())
-    signal_strength = db.Column(db.Float())
+    max_power = db.Column(db.Float)
+    consumtion_fuse = db.Column(db.Integer)
+    maincabinet_rating = db.Column(db.Integer)
+    empty_fuses = db.Column(db.Boolean)
+    number_of_slots = db.Column(db.Integer)
+    signal_strength = db.Column(db.Float)
     installation_location = db.Column(db.String(255))
 
     # Foreign keys to User and Charger models
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
-    charger_id = db.Column(db.Integer(), db.ForeignKey('charger.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    charger_id = db.Column(db.Integer, db.ForeignKey('charger.id'))
+
+    # Many-to-Many relationships
+    contact_person = db.relationship('ContactPerson', secondary=survey_contact_rel, backref='surveys', lazy=True)
 
     def __repr__(self):
         return f""" Survey <{self.id} |{self.grid_connection} |{self.grid_cable} | {self.max_power} |
                             {self.maincabinet_rating}>"""
 
 class Charger(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     manufacturer = db.Column(db.String(30), nullable=False)
     model = db.Column(db.String(30), nullable=False)
     product_no = db.Column(db.String(30), nullable=False, unique=True)
-    price = db.Column(db.Float(), nullable=False)
+    price = db.Column(db.Float, nullable=False)
     type_of_outlet = db.Column(db.String(10), nullable=False)
-    no_of_outlets = db.Column(db.Integer()) # Number of outlets in the charger
+    no_of_outlets = db.Column(db.Integer) # Number of outlets in the charger
     dc_ac = db.Column(db.String(2), nullable=False)
     communication = db.Column(db.String(20), nullable=False)
     mounting = db.Column(db.String(50), nullable=False) # How charger is mounted in place (wall, ground etc)
-    max_power = db.Column(db.Float(), nullable=False)
-    mcb = db.Column(db.Boolean(), nullable=False)
-    rcd_typea = db.Column(db.Boolean(), nullable=False)
-    rcd_typeb = db.Column(db.Boolean(), nullable=False)
-    automatic_rcd = db.Column(db.Boolean(), nullable=False)
-    pwr_outage_eq = db.Column(db.Boolean(), nullable=False)
-    mid_meter = db.Column(db.Boolean(), nullable=False)
-    mid_readabe = db.Column(db.Boolean(), nullable=False) # Is the MID-meter readable from outside w/o tools
-    max_cable_d = db.Column(db.Integer(), nullable=False)
+    max_power = db.Column(db.Float, nullable=False)
+    mcb = db.Column(db.Boolean, nullable=False)
+    rcd_typea = db.Column(db.Boolean, nullable=False)
+    rcd_typeb = db.Column(db.Boolean, nullable=False)
+    automatic_rcd = db.Column(db.Boolean, nullable=False)
+    pwr_outage_eq = db.Column(db.Boolean, nullable=False)
+    mid_meter = db.Column(db.Boolean, nullable=False)
+    mid_readabe = db.Column(db.Boolean, nullable=False) # Is the MID-meter readable from outside w/o tools
+    max_cable_d = db.Column(db.Integer, nullable=False)
 
     # Backref to survey to see which chargers are used in which surveys
     charger_id = db.relationship('Survey', backref='charger', lazy='dynamic')
@@ -127,22 +128,25 @@ class Location(db.Model):
     postal_code = db.Column(db.String(10), nullable=False)
     city = db.Column(db.String(30), nullable=False)
     country = db.Column(db.String(30), nullable=False)
-    coordinate_lat = db.Column(db.Float())
-    coordinate_long = db.Column(db.Float())
+    coordinate_lat = db.Column(db.Float)
+    coordinate_long = db.Column(db.Float)
+
+    # Foreign keys
+    survey_id = db.Column(db.Integer, db.ForeignKey('survey.id'))
 
     def __repr__(self):
         return f"""Location <{self.name} |{self.address} |{self.postal_code} |{self.city} |
                             {self.country}>"""
 
-class OrgType(db.Model):
+class Orgtype(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(40), nullable=False)
     description = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
-        return f'OrgType <{self.title} | {self.description}>'
+        return f'Organization type <{self.title} | {self.description}>'
 
-class ContactPerson(db.Model):
+class Contactperson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
