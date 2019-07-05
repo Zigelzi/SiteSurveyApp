81c5fd80c382252dc7c095c096eda630296da104
Zigelzi <40773205+Zigelzi@users.noreply.github.com>
2019-07-05 17:16:03 +0300

    Added blueprint registrations to main package __init__.py file

--- sitesurvey/__init__.py
+++ sitesurvey/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 
-from sitesurvey.config import Config, DevConfig
+from sitesurvey.main.config import Config, DevConfig
 
 # Initializing the app, database and bcrypt (hashing)
 app = Flask(__name__)
@@ -20,4 +20,15 @@ login_manager.login_message = 'Log in to access this page'
 login_manager.session_protection = 'strong'
 data_req_msg = 'Required field' # Variable for error message shown in FlaskForms
 
-from sitesurvey import routes
+from sitesurvey.auth.routes import bp_auth
+from sitesurvey.charger.routes import bp_charger
+from sitesurvey.main.routes import bp_main
+from sitesurvey.survey.routes import bp_survey
+from sitesurvey.user.routes import bp_user
+
+app.register_blueprint(bp_auth)
+app.register_blueprint(bp_charger)
+app.register_blueprint(bp_main)
+app.register_blueprint(bp_survey)
+app.register_blueprint(bp_user)
+
