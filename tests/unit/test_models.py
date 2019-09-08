bad647fbd7c8e8c59ef67fbe841c1b8aa270d706
Zigelzi <40773205+Zigelzi@users.noreply.github.com>
2019-09-08 12:16:10 +0300

    Created product package unittests and started survey package unittests

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -6,12 +6,93 @@ blueprint package (folder) name under the main sitesurvey package.
 import os
 import sys
 import pytest
+from datetime import datetime
 
 parent_dir = os.path.dirname
 
 # Add the package root directory to sys.path so imports work
 sys.path.append(parent_dir(parent_dir(parent_dir(os.path.abspath(__file__)))))
 
+
+
+"""
++ ---------------------------------------- +
+| PRODUCT PACKAGE MODELS                   |
+| Unit tests for sitesurvey.product.models |
++ ---------------------------------------- +
+"""
+from sitesurvey.product.models import Product, Productcategory
+
+def test_new_product():
+    """
+    GIVEN a Product model
+    WHEN a new Product is created
+    THEN check that the product_number, product_name, unit_of_material and price fields are defined correctly
+    """
+    product = Product(product_number='CND-1000',
+                      product_name='Product of testing',
+                      unit_of_material='pcs' ,
+                      price=1234.99)
+    assert product.product_number == 'CND-1000'
+    assert product.product_name == 'Product of testing'
+    assert product.unit_of_material == 'pcs'
+    assert product.price == 1234.99
+
+def test_new_productcategory():
+    """
+    GIVEN a Productcategory model
+    WHEN a new Productcategory is created
+    THEN check that the title and description fields are defined correctly
+    """
+    product_category = Productcategory(title='Charger', description='Charger product group for testings')
+    assert product_category.title == 'Charger'
+    assert product_category.description == 'Charger product group for testings'
+
+"""
++ ---------------------------------------- +
+| SURVEY PACKAGE MODELS                    |
+| Unit tests for sitesurvey.survey.models  |
++ ---------------------------------------- +
+"""
+from sitesurvey.survey.models import Survey
+
+def test_new_survey():
+    now = datetime.utcnow()
+    survey = Survey(create_date=now,
+                    update_date=now,
+                    status='created',
+                    installation_method='Ground',
+                    concrete_foundation=False,
+                    grid_connection=63,
+                    grid_cable='AXMK 50+50',
+                    max_power=33.2,
+                    consumption_fuse=63,
+                    maincabinet_rating=250,
+                    empty_fuses=True,
+                    number_of_slots=2,
+                    signal_strength=-33.2,
+                    installation_location='Testing parking garage at parking hall P2')
+    assert survey.create_date == now
+    assert survey.status == 'created'
+    assert survey.installation_method == 'Ground'
+    assert survey.concrete_foundation == False
+    assert survey.grid_connection == 63
+    assert survey.grid_cable == 'AXMK 50+50'
+    assert survey.max_power == 33.2
+    assert survey.consumption_fuse == 63
+    assert survey.maincabinet_rating == 250
+    assert survey.empty_fuses == True
+    assert survey.number_of_slots == 2
+    assert survey.signal_strength == -33.2
+    assert survey.installation_location == 'Testing parking garage at parking hall P2'
+
+
+""" 
++ ---------------------------------------- +
+| USER PACKAGE MODELS                      |
+| Unit tests for sitesurvey.product.models |
++ ---------------------------------------- +
+"""
 from sitesurvey.user.models import User, Organization, Orgtype, Contactperson
 
 def test_new_user():
